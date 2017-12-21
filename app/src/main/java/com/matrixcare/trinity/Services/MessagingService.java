5a14d6cf5888ccf839b182464deb6efaa252a2ab
ryan-lenoci <ryan.lenoci@matrixcare.com>
2017-12-20 21:12:46 -0500

    More tweaks to the MessagingService.

--- app/src/main/java/com/matrixcare/trinity/Services/MessagingService.java
+++ app/src/main/java/com/matrixcare/trinity/Services/MessagingService.java
@@ -1,5 +1,6 @@
 package com.matrixcare.trinity.Services;
 
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -13,6 +14,10 @@ import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 import com.matrixcare.trinity.Activities.MainActivity;
 import com.matrixcare.trinity.R;
+
+import org.joda.time.DateTime;
+
+import java.util.Map;
 
 /**
  * Created by rlenoci on 12/20/2017.
@@ -28,21 +33,40 @@ public class MessagingService extends FirebaseMessagingService {
         if(remoteMessage.getData().size() > 0)
         {
             Log.d(TAG, "Message data: " + remoteMessage.getData());
-            sendNotification(remoteMessage.getData().get("body"));
+            Map<String, String> data = remoteMessage.getData();
+            String ClientId = data.get("FirstName");
+            String FirstName = data.get("FirstName");
+            String LastName = data.get("LastName");
+            String StartTime = data.get("StartTime");
+            String Address1 = data.get("Address1");
+            String Address2 = data.get("Address2");
+            String City = data.get("City");
+            String StateOrProvince = data.get("StateOrProvince");
+            String PostalCode = data.get("PostalCode");
+            String Country = data.get("Country");
+
+            String smallMessage = "Open Shift at " + getPrintedDate(StartTime);
+            String bigMessage = "Time: " +  getPrintedDate(StartTime) +
+                    "\nClient: " + FirstName + " " + LastName +
+                    "\nAddress: " + Address1 + " " + Address2 + " " + City + ", " + StateOrProvince;
+
+            sendNotification(smallMessage, bigMessage);
         }
 
         if(remoteMessage.getNotification() != null)
         {
             Log.d(TAG, "Message body: " + remoteMessage.getNotification().getBody());
-            sendNotification(remoteMessage.getNotification().getBody());
+            //sendNotification(remoteMessage.getNotification().getBody());
         }
     }
 
 
-    private void sendNotification(String body) {
+
+
+    private void sendNotification(String smallMessage, String bigMessage) {
         Intent intent = new Intent(this, MainActivity.class);
 
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
 
         NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
         NotificationCompat.Builder notificationBuilder;
@@ -54,21 +78,32 @@ public class MessagingService extends FirebaseMessagingService {
             notificationChannel.setDescription("Caregiver Request Notifications");
             notificationManager.createNotificationChannel(notificationChannel);
 
-
             notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
                     .setSmallIcon(R.mipmap.ic_launcher)
                     .setContentTitle("Caregiver Request")
-                    .setContentText(body)
-                    .setContentIntent(pendingIntent);
+                    .setContentText(smallMessage)
+                    .setContentIntent(pendingIntent)
+                    .setStyle(new NotificationCompat.BigTextStyle().bigText(bigMessage))
+                    .addAction (0, "Accept", pendingIntent)
+                    .addAction (0, "Dismiss", null);
         }
         else
         {
             notificationBuilder = new NotificationCompat.Builder(this)
                     .setSmallIcon(R.mipmap.ic_launcher)
                     .setContentTitle("Caregiver Request")
-                    .setContentText(body)
+                    .setContentText(smallMessage)
                     .setContentIntent(pendingIntent);
         }
-        notificationManager.notify(0, notificationBuilder.build());
+        Notification notification = notificationBuilder.build();
+        notification.flags |= Notification.FLAG_AUTO_CANCEL;
+        notificationManager.notify(0, notification);
     }
-}
+
+    public String getPrintedDate(String dateTimeString) {
+        DateTime dateTime = DateTime.parse(dateTimeString);
+        return dateTime.toString("MM/dd/yyyy K:mm a");
+    }
+
+
+    }
